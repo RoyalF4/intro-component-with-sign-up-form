92d0afdb43d1a5ce1dcd923da4d1128021c8b4e7
Royal Finch <Royal2791@gmail.com>
2024-05-11 08:53:15 -0400

    feat: :sparkles: add basic html structure/content

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,41 @@
-export default function App() {}
+export default function App() {
+  return (
+    <main className="main">
+      <h1 className="heading">Learn to code by watching others</h1>
+      <p className="subtext">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+      <a href="#">Try it free 7 days then $20/mo. thereafter</a>
+      <form action="" className="form">
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          className="input-field"
+        />
+        <input
+          type="text"
+          name="secondName"
+          id="secondName"
+          className="input-field"
+        />
+        <input type="email" name="email" id="email" className="input-field" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="input-field"
+        />
+        <a href="#">Claim your free trial </a>
+        <span className="disclaimer">
+          By clicking the button, you are agreeing to our Terms and Services
+        </span>
+      </form>
+    </main>
+  );
+}
 
 /* 
   Learn to code by watching others
